27b7e34e884344aaec6f2f2d4d4c7df196d0b284
TudoHuang <88125758+tudohuang@users.noreply.github.com>
2024-12-01 17:35:52 +0800

    Create p2.py

---
+++ day1/p2.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+def calculate_similarity_score(file_path):
+    with open(file_path, 'r') as file:
+        pairs = [list(map(int, line.split())) for line in file]
+    
+
+    left_list, right_list = zip(*pairs)
+    
+    right_counts = Counter(right_list)
+    
+
+    similarity_score = sum(num * right_counts[num] for num in left_list)
+    return similarity_score
+
+
+file_path = 'day1/d1.txt'
+
+
+print(f"相似度得分: {calculate_similarity_score(file_path)}")
